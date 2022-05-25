9eda4fb84d6085ed1e965ca6be9ac7467e7e7df0
Al <alastairpatrick@gmail.com>
2022-05-25 16:11:18 -0700

    Map HID key codes to ASCII

--- hid.c
+++ hid.c
@@ -3,27 +3,50 @@
 #include "sys80.h"
 
 #define MAX_REPORT  4
+#define NUM_HID_CODES 128
 
 extern const uint8_t g_key_map[0x68];
+static const char g_ascii_map[NUM_HID_CODES][2] =  { HID_KEYCODE_TO_ASCII };
+static hid_keyboard_report_t g_last_kbd_report;
 
 void MapModifierKeys(uint8_t* rows, int modifiers);
 
-static struct
-{
+static struct {
   uint8_t report_count;
   tuh_hid_report_info_t report_info[MAX_REPORT];
 } g_hid_info[CFG_TUH_HID];
 
-static void ProcessKeyboardReport(hid_keyboard_report_t const *report)
-{  
+static bool WasKeyPressed(int code) {
+  for (int i = 0; i < count_of(g_last_kbd_report.keycode); ++i) {
+    if (g_last_kbd_report.keycode[i] == code) {
+      return true;
+    }
+  }
+  return false;
+}
+
+static void KeyPressed(int code, int modifiers) {
+  static char foo;
+  if (code >= NUM_HID_CODES)
+    return;
+
+  bool is_shift = modifiers & (KEYBOARD_MODIFIER_LEFTSHIFT | KEYBOARD_MODIFIER_RIGHTSHIFT);
+  char ascii = g_ascii_map[code][is_shift ? 1 : 0];
+
+  (void) ascii;
+  // TODO: pass ascii to the terminal.
+}
+
+static void ProcessKeyboardReport(hid_keyboard_report_t const *report) {
   uint8_t rows[KEYBOARD_ROWS] = {0};
   for (int i = 0; i < count_of(report->keycode); ++i) {
-    int usb_code = report->keycode[i];
-    if (usb_code >= count_of(g_key_map)) {
+    // Map to kayboard matrix.
+    int hid_code = report->keycode[i];
+    if (hid_code >= count_of(g_key_map)) {
       continue;
     }
 
-    int mapped_code = g_key_map[usb_code];
+    int mapped_code = g_key_map[hid_code];
     int col = mapped_code & 0xF;
     int row = mapped_code >> 4;
     if (row >= KEYBOARD_ROWS) {
@@ -31,6 +54,11 @@ static void ProcessKeyboardReport(hid_keyboard_report_t const *report)
     }
 
     rows[row] |= 1 << col;
+
+    // New key press?
+    if (!WasKeyPressed(hid_code)) {
+      KeyPressed(hid_code, report->modifier);
+    }
   }
 
   MapModifierKeys(rows, report->modifier);
@@ -38,37 +66,33 @@ static void ProcessKeyboardReport(hid_keyboard_report_t const *report)
   for (int i = 0; i < KEYBOARD_ROWS; ++i) {
     g_sys80_regs.kbd_rows[i] = rows[i];
   }
+
+  g_last_kbd_report = *report;
 }
 
-static void ProcessMouseReport(hid_mouse_report_t const * report)
-{
+static void ProcessMouseReport(hid_mouse_report_t const * report) {
   g_sys80_regs.mouse_buttons = report->buttons;
   g_sys80_regs.mouse_x += report->x;
   g_sys80_regs.mouse_y += report->y;
 }
 
-static void ProcessGenericReport(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
-{
+static void ProcessGenericReport(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
   (void) dev_addr;
 
   uint8_t const rpt_count = g_hid_info[instance].report_count;
   tuh_hid_report_info_t* rpt_info_arr = g_hid_info[instance].report_info;
   tuh_hid_report_info_t* rpt_info = NULL;
 
-  if ( rpt_count == 1 && rpt_info_arr[0].report_id == 0)
-  {
+  if (rpt_count == 1 && rpt_info_arr[0].report_id == 0) {
     // Simple report without report ID as 1st byte
     rpt_info = &rpt_info_arr[0];
-  }else
-  {
+  } else {
     // Composite report, 1st byte is report ID, data starts from 2nd byte
     uint8_t const rpt_id = report[0];
 
     // Find report id in the arrray
-    for(uint8_t i=0; i<rpt_count; i++)
-    {
-      if (rpt_id == rpt_info_arr[i].report_id )
-      {
+    for (uint8_t i=0; i<rpt_count; i++) {
+      if (rpt_id == rpt_info_arr[i].report_id) {
         rpt_info = &rpt_info_arr[i];
         break;
       }
@@ -78,8 +102,7 @@ static void ProcessGenericReport(uint8_t dev_addr, uint8_t instance, uint8_t con
     len--;
   }
 
-  if (!rpt_info)
-  {
+  if (!rpt_info) {
     return;
   }
 
@@ -90,18 +113,16 @@ static void ProcessGenericReport(uint8_t dev_addr, uint8_t instance, uint8_t con
   // - Consumer Control (Media Key) : Consumer, Consumer Control
   // - System Control (Power key)   : Desktop, System Control
   // - Generic (vendor)             : 0xFFxx, xx
-  if ( rpt_info->usage_page == HID_USAGE_PAGE_DESKTOP )
-  {
-    switch (rpt_info->usage)
-    {
+  if (rpt_info->usage_page == HID_USAGE_PAGE_DESKTOP) {
+    switch (rpt_info->usage) {
       case HID_USAGE_DESKTOP_KEYBOARD:
         // Assume keyboard follow boot report layout
-        ProcessKeyboardReport( (hid_keyboard_report_t const*) report );
+        ProcessKeyboardReport((hid_keyboard_report_t const*) report);
         break;
 
       case HID_USAGE_DESKTOP_MOUSE:
         // Assume mouse follow boot report layout
-        ProcessMouseReport( (hid_mouse_report_t const*) report );
+        ProcessMouseReport((hid_mouse_report_t const*) report);
         break;
 
       default:
@@ -110,43 +131,37 @@ static void ProcessGenericReport(uint8_t dev_addr, uint8_t instance, uint8_t con
   }
 }
 
-void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len)
-{
+void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len) {
   // Interface protocol (hid_interface_protocol_enum_t)
   uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
 
   // By default host stack will use activate boot protocol on supported interface.
   // Therefore for this simple example, we only need to parse generic report descriptor (with built-in parser)
-  if ( itf_protocol == HID_ITF_PROTOCOL_NONE )
-  {
+  if (itf_protocol == HID_ITF_PROTOCOL_NONE) {
     g_hid_info[instance].report_count = tuh_hid_parse_report_descriptor(g_hid_info[instance].report_info, MAX_REPORT, desc_report, desc_len);
   }
 
   // request to receive report
   // tuh_hid_report_received_cb() will be invoked when report is available
-  if ( !tuh_hid_receive_report(dev_addr, instance) )
-  {
+  if (!tuh_hid_receive_report(dev_addr, instance)) {
     assert(false);
   }
 }
 
 // Invoked when device with hid interface is un-mounted
-void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
-{
+void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance) {
 }
 
-void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len)
-{
+void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* report, uint16_t len) {
   uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
 
-  switch (itf_protocol)
-  {
+  switch (itf_protocol) {
     case HID_ITF_PROTOCOL_KEYBOARD:
-      ProcessKeyboardReport( (hid_keyboard_report_t const*) report );
+      ProcessKeyboardReport((hid_keyboard_report_t const*) report);
       break;
 
     case HID_ITF_PROTOCOL_MOUSE:
-      ProcessMouseReport( (hid_mouse_report_t const*) report );
+      ProcessMouseReport((hid_mouse_report_t const*) report);
       break;
 
     default:
@@ -156,8 +171,7 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
   }
 
   // continue to request to receive report
-  if ( !tuh_hid_receive_report(dev_addr, instance) )
-  {
+  if (!tuh_hid_receive_report(dev_addr, instance)) {
     assert(false);
   }
 }
